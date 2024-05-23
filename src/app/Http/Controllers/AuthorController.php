33ba3fb3959f550c1700e0dbd27cd07e1c7d5356
hiroyuki hosoda <158367633+Hiroyukihosoda@users.noreply.github.com>
2024-05-23 17:32:55 +0900

    バリデーション実装

--- src/app/Http/Controllers/AuthorController.php
+++ src/app/Http/Controllers/AuthorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Author;
+use App\Http\Requests\AuthorRequest;
 
 class AuthorController extends Controller
 {
@@ -16,7 +17,7 @@ class AuthorController extends Controller
         return view('add');
     }
 
-    public function create(Request $request) {
+    public function create(AuthorRequest $request) {
         $form = $request->all();
         Author::create($form);
         return redirect('/');
@@ -27,7 +28,7 @@ class AuthorController extends Controller
         return view('edit', ['form' => $author]);
     }
 
-    public function update(Request $request) {
+    public function update(AuthorRequest $request) {
         $form = $request->all();
         unset($form['_token']);
         Author::find($request->id)->update($form);
@@ -55,5 +56,17 @@ class AuthorController extends Controller
             'item' => $item
         ];
         return view('find', $param);
+    }
+
+    public function bind(Author $author) {
+        $data = [
+            'item' => $author,
+        ];
+        return view('author.binds', $data);
+    }
+
+     public function verror()
+    {
+    return view('verror');
     }
 }
